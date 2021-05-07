2d4d97baafc374c1495af5a54d60c2a1f02aad60
Cedric Bös <cedric.boes@online.de>
2021-05-07 13:41:10 +0200

    feat: Add basic structure of MAVLink safer module

---
+++ src/main/java/de/wuespace/telestion/extension/mavlink/safer/MavlinkFile.java
@@ -0,0 +1,18 @@
+package de.wuespace.telestion.extension.mavlink.safer;
+
+public final class MavlinkFileHandler {
+    public static void load() {
+
+    }
+
+    public static void write() {
+
+    }
+
+    /**
+     * 
+     */
+    private MavlinkFileHandler() {
+
+    }
+}

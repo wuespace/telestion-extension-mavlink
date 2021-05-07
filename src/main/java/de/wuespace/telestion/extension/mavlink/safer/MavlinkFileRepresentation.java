ec2afbff83f6d9a2cc976a32262daf74487acbe8
Cedric Bös <cedric.boes@online.de>
2021-05-07 13:48:34 +0200

    feat: Add functionality to MAVLink safer module

--- src/main/java/de/wuespace/telestion/extension/mavlink/safer/MavlinkFileRepresentation.java
+++ src/main/java/de/wuespace/telestion/extension/mavlink/safer/MavlinkFileRepresentation.java
@@ -1,2 +1,8 @@
-package de.wuespace.telestion.extension.mavlink.safer;public class MavlinkFileRepresentation {
+package de.wuespace.telestion.extension.mavlink.safer;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import de.wuespace.telestion.api.message.JsonMessage;
+
+public record MavlinkFileRepresentation(@JsonProperty
+										MavlinkFileEntry[] entries) implements JsonMessage {
 }
